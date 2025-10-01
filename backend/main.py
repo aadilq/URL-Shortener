076d8692b58d9194ac4cfce435c828a2c5aade14
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-10-01 09:21:30 -0400

    define url request model

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
+class URL_Request(BaseModel):
+    url: HttpUrl
+
+## Defines what we want to send back to the user
 
 @app.get("/")
 async def root():
     return {"message": "URL Shortener API is running!"}
+
 
