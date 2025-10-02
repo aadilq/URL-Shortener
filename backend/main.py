bd277e832aa7ce50eb0e98007a2ceae4748d5e89
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-10-02 17:23:22 -0400

    define post route to generate short code

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ import random
 
 app = FastAPI()
 
+url_database = {}
+
 ## Request Model - Defines what type of data we expect from the user
 class URL_Request(BaseModel):
     url: HttpUrl ## pydantic HttpUrl allows us to validate if the url is a proper url
@@ -25,6 +27,24 @@ def generate_short_code(length=6):
 @app.get("/")
 async def root():
     return {"message": "URL Shortener API is running!"}
+
+
+@app.post("/api/shorten_url", response_model=URL_Response)
+def shorten_url(request: URL_Request):
+    short_code = generate_short_code()
+
+    while short_code in url_database:
+        short_code = generate_short_code
+    
+
+    url_database[short_code] = str(request.url)
+
+    return{
+        "short_code": short_code, 
+        "original_url": str(request.url),
+        "short_url" : f"http://localhost:8000/{short_code}"
+    }
+
 
 
 
